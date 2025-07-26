f4c84b25b25de434b774ad17bebfbace89651baa
Abel Velasquez <92559628+abelvelasquezchavez@users.noreply.github.com>
2025-07-25 22:27:15 -0500

    feat: agrega index.html público y configura Express para servirlo

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 
 import express from 'express';
 import reglaRoutes from './routes/reglas.routes';
+import path from 'path';
 // @ts-ignore
 import cors from 'cors';
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 // Configuración básica de CORS
 const corsOptions = {
-  origin: true,
-  credentials: true
+    origin: true,
+    credentials: true
 };
 
 app.use(cors(corsOptions));
